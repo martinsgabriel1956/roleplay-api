a10a3941c48d32e8e6b493bcc2748625db91180c
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-11-02 16:22:49 -0300

    feat: user model generated and created

--- tests/users/user.spec.ts
+++ tests/users/user.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@japa/runner'
 
 test.group('User', () => {
-  test('it should create a user', async ({ client, assert }) => {
+  test('it should create a user', async ({ client }) => {
     const userPayload = {
       email: 'test@test.com',
       username: 'test',
