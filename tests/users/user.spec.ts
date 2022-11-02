ad45cd1acd391e4d1fea4c6aeaa4ef8c4e50e38b
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-11-02 14:28:03 -0300

    feat: user data defined

--- tests/users/user.spec.ts
+++ tests/users/user.spec.ts
@@ -1,15 +1,17 @@
 import { test } from '@japa/runner'
 
 test.group('User', () => {
-  test('it should create a user', async ({ client }) => {
+  test('it should create a user', async ({ client, assert }) => {
     const userPayload = {
       email: 'test@test.com',
       username: 'test',
       password: 'test',
+      avatar: 'https://images.com/avatar/1',
     }
 
     const response = await client.post('/users').json(userPayload)
+    const { password, ...expected } = userPayload
 
-    response.assertStatus(201)
+    response.assertBodyContains(expected)
   })
 })
