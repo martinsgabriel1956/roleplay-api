6ad2115505575aae93785cb5f48a42abcd18398c
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-11-07 18:49:05 -0300

    feat: adonis mailer installed and configurated and Mail adonis mudule in use

--- tests/auth/passwords.spec.ts
+++ tests/auth/passwords.spec.ts
@@ -1,10 +1,13 @@
 import Database from '@ioc:Adonis/Lucid/Database'
+import Mail from '@ioc:Adonis/Addons/Mail'
 import { test } from '@japa/runner'
 import UserFactory from 'Database/factories/UserFactory'
 
 test.group('Passwords', (group) => {
-  test('it should send email with forgot password instructions', async ({ client }) => {
+  test('it should send email with forgot password instructions', async ({ client, assert }) => {
     const user = await UserFactory.create()
+
+    const mailer = Mail.fake('smtp')
 
     const response = await client.post('/forgot-password').json({
       email: user.email,
@@ -12,6 +15,8 @@ test.group('Passwords', (group) => {
     })
 
     response.assertStatus(204)
+
+    Mail.restore()
   })
 
   group.each.setup(async () => {
