efb988c92f6a1546fd7b94bab518964d0e9e4b94
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-11-07 15:57:05 -0300

    feat: update user password test implemented and tested

--- tests/users/update-user.spec.ts
+++ tests/users/update-user.spec.ts
@@ -1,9 +1,10 @@
 import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
 import UserFactory from 'Database/factories/UserFactory'
+import Hash from '@ioc:Adonis/Core/Hash'
 
 test.group('Update User', (group) => {
-  test('it should update an user', async ({ client }) => {
+  test('it should update an user', async ({ client, assert }) => {
     const { id, password } = await UserFactory.create()
     const email = 'test@test.com'
     const avatar = 'http://github.com/martinsgabriel1956.png'
@@ -22,6 +23,27 @@ test.group('Update User', (group) => {
         id,
       },
     })
+  })
+
+  test('it should update the password of the user', async ({ assert, client }) => {
+    const user = await UserFactory.create()
+    const password = 'test'
+
+    const response = await client.put(`/users/${user.id}`).json({
+      email: user.email,
+      avatar: user.avatar,
+      password,
+    })
+
+    response.assertStatus(200)
+    response.assertBodyContains({
+      user: {
+        id: user.id,
+      },
+    })
+
+    await user.refresh()
+    assert.isTrue(await Hash.verify(user.password, password))
   })
 
   group.each.setup(async () => {
